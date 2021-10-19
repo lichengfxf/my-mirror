86f4d60da7389d1bd1c10f20b3251976ac3a2251
lichengfxf <lichengfxf@163.com>
2021-10-19 08:23:48 +0800

    增加run.py

--- UI/MainFrame.py
+++ UI/MainFrame.py
@@ -1,12 +1,12 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QDialog, QWidget
-from Wnd.MainDlg import *
- 
-if __name__ == '__main__':
+from PyQt5.QtWidgets import QApplication
+from UI.Wnd.MainDlg import *
+
+# 执行
+def run():
     app = QApplication(sys.argv)
 
     w = MainDlg()
     w.show()
 
     sys.exit(app.exec_())
-
---
+++ run.py
@@ -0,0 +1,4 @@
+from UI import MainFrame
+
+# 加载界面
+MainFrame.run()
