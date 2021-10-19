774f6360d6a79f8e4e13a9b2ec5daa899899af05
lichengfxf <lichengfxf@163.com>
2021-10-19 21:52:12 +0800

    增加发布脚本

---
+++ 发布.bat
@@ -0,0 +1,3 @@
+@REM D:\Program Files\anaconda\Scripts\pyinstaller.exe
+
+"D:\Program Files\anaconda\Scripts\pyinstaller.exe" -n mm.exe -F -w --specpath "Release" --distpath "Release" --workpath "Release\tmp" run.py
